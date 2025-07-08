f5d535b85cf53f7813dfe056be3f32b530bc28fc
RAMTANI <ramtani@MacBook-Air-de-RAMTANI.local>
2025-07-09 01:52:14 +0200

    Generating the frontend setup

---
+++ mobile/app/about.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ import { Text, View } from 'react-native'
+ function About() {
+   return (
+     <View>
+       <Text>About Page</Text>
+     </View>
+   )
+ }
+ 
+ export default About
+ 
